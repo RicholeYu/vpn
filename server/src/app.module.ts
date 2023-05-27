be10710b440a2b1d0f2b2f492ca7db5d915ac8ca
Richole Yu <richole.yu@prudential.com.cn>
2023-05-27 14:35:53 +0800

    .

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path'
-console.log(join(__dirname, '../../', 'go/dist'))
+import { join } from 'path';
+console.log(join(__dirname, '../../', 'go/dist'));
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'go/dist'),
-      serveRoot: '/go'
-    })
+      serveRoot: '/go',
+    }),
   ],
   controllers: [AppController],
 })
